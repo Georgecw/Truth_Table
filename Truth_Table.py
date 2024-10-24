30df33ca42d255854a7b382dee0430548d19f421
Chen Jiazhi <51824647+Georgecw@users.noreply.github.com>
2024-10-24 16:55:58 +0800

    实现基本功能
    可以通过用户输入生成真值表(csv 文件)

---
+++ Truth_Table.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import numpy as np
+
+
+def make_table(trues, dim):
+    dim = int(dim)
+
+    variables = []
+    for i in range(0, dim):
+        variables.append("A" + str(i))
+    variables.append("Y")
+
+    data = np.zeros((2**dim, dim + 1))
+    for i in range(0, 2**dim):
+        bi = bin(i)
+        bi_arr = list(str(bi))
+        del bi_arr[0:2]
+        bi_arr = [int(bi) for bi in bi_arr]
+        for j in range(0, dim - len(bi_arr)):
+            bi_arr.insert(0, 0)
+        bi_arr.append(0)
+        bi_np = np.array(bi_arr)
+        data[i] = bi_np
+    print(data)
+
+    table = pd.DataFrame(data=data, columns=variables, dtype=int)
+
+    table.iloc[trues, [dim]] = 1
+
+    return table
+
+
+trues = input("请输入值为 1 的最小项, 用逗号分隔: ").split(",")
+trues = [int(num) for num in trues]
+table = make_table(trues, 4)
+table.to_csv("Truth_Table.csv", index=False)
